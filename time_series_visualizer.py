2b62580df6c8a0b84666965bed1e15ce681498b9
Avery Barrera <95246892+abarriebee@users.noreply.github.com>
2022-05-07 14:26:13 -0500

    Create time_series_visualizer.py

---
+++ time_series_visualizer.py
@@ -0,0 +1,44 @@
+df = None
+
+
+def draw_line_plot():
+    # Draw line plot
+
+
+
+
+
+    # Save image and return fig (don't change this part)
+    fig.savefig('line_plot.png')
+    return fig
+
+def draw_bar_plot():
+    # Copy and modify data for monthly bar plot
+    df_bar = None
+
+    # Draw bar plot
+
+
+
+
+
+    # Save image and return fig (don't change this part)
+    fig.savefig('bar_plot.png')
+    return fig
+
+def draw_box_plot():
+    # Prepare data for box plots (this part is done!)
+    df_box = df.copy()
+    df_box.reset_index(inplace=True)
+    df_box['year'] = [d.year for d in df_box.date]
+    df_box['month'] = [d.strftime('%b') for d in df_box.date]
+
+    # Draw box plots (using Seaborn)
+
+
+
+
+
+    # Save image and return fig (don't change this part)
+    fig.savefig('box_plot.png')
+    return fig
